2027be83bde159eba5d67c55fafdfe6b425ccca6
Ian Rose <ian.r.rose@gmail.com>
2019-02-19 10:30:14 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * CLI script to transform notebook mimebundles to text/html
+ * using jupyterlab renderers.
+ */
+
+const json = require('@jupyterlab/json-extension');
+const vega4 = require('@jupyterlab/vega4-extension');
+const vdom = require('@jupyterlab/vdom-extension');
