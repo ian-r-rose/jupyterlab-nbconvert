8105fa9185212784667ad383e5f185cdd3991369
Ian Rose <ian.r.rose@gmail.com>
2019-02-19 12:32:23 -0800

    Work on converter.

--- index.js
+++ index.js
@@ -3,6 +3,24 @@
  * using jupyterlab renderers.
  */
 
+const process = require('process');
+require('jsdom-global')();
+global.navigator = {
+  platform: process.platform,
+  userAgent: 'Node'
+};
+
 const json = require('@jupyterlab/json-extension');
 const vega4 = require('@jupyterlab/vega4-extension');
 const vdom = require('@jupyterlab/vdom-extension');
+const fs = require('fs');
+
+
+function convert(bundle) {
+  return bundle;
+}
+
+
+const data = fs.readFileSync(0);
+const converted = convert(data);
+console.log(converted);
--- src/browser.ts
+++ src/browser.ts
@@ -6,14 +6,31 @@ import { RenderMimeRegistry } from '@jupyterlab/rendermime';
 
 import { ReadonlyJSONObject } from '@phosphor/coreutils';
 
+import { Widget } from '@phosphor/widgets';
+
 const registry = new RenderMimeRegistry();
 registry.addFactory(json.rendererFactory);
 registry.addFactory(vega.rendererFactory);
 registry.addFactory(vdom.rendererFactory);
 
+const metadata: { [x: string]: ReadonlyJSONObject } = {
+  'application/json': { expanded: true }
+};
 
-function convert(data: ReadonlyJSONObject): ReadonlyJSONObject {
+
+async function convert(data: ReadonlyJSONObject): Promise<ReadonlyJSONObject> {
+  for (let mimeType of Object.keys(data)) {
+    if(registry.getFactory(mimeType)) {
+      const renderer = registry.createRenderer(mimeType);
+      Widget.attach(renderer, document.body);
+      const model = registry.createModel({ data, metadata: metadata[mimeType] });
+      await renderer.renderModel(model);
+      let newData = { 'text/html': renderer.node.innerHTML };
+      Widget.detach(renderer);
+      return newData;
+    }
+  }
   return data;
 }
 
-window.convert = convert;
+(window as any).convert = convert;
